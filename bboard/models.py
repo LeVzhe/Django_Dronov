1af5df143d2d22ac8fcb734bf022e8a37615c933
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-06-14 20:12:07 +0300

    addl: rename model

--- bboard/models.py
+++ bboard/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    price = models.FloatField()
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+#verbose_name - параметр, для человеческого названия полей модели
+    title = models.CharField(max_length=50, verbose_name='Toвар')
+    content = models.TextField(verbose_name='Описание')
+    price = models.FloatField(verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+
+#человеческое название самой модели
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-published']
