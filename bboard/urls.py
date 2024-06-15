7858569631c8eff598bfdd6ce82f0cd8db1bfb10
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-06-15 15:24:24 +0300

    addfs: basic.html, forms.py; addl: start layout logic, create form page

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, rubric_bbs
+from .views import index, rubric_bbs, BbcreateView
 
 urlpatterns = [
     path('<int:rubric_id>/', rubric_bbs, name='rubric_bbs'),
     path('', index, name='index'),
+    path('add/', BbcreateView.as_view(), name='add')
 ]
